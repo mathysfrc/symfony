d919555d273541b1953c288e283a628aec354c4b
mathysfrc <mathysf41@gmail.com>
2023-05-22 12:55:56 +0200

    modif

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,25 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgramController extends AbstractController
+{
+    #[Route('/program/', name: 'program_index')]
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+         ]);
+    }
+
+         #[Route('/program/{id}', methods:['GET'], requirements: ['id'=>'\d+'], name:'program_id')]
+         public function show(int $id): Response
+         {
+             return $this->render('program/show.html.twig', ['id' => $id]);
+         }
+    
+}
